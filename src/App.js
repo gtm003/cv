75ce225340b550b8f08a92ab160c52bfdf7e08eb
Grigorovich Tatyana <gtm003@gmail.com>
2021-03-21 16:16:32 +0300

    init: ctreate and publish cv

--- src/App.js
+++ src/App.js
@@ -4,20 +4,63 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <main >
+        <img src="https://i.postimg.cc/7P7GbsL3/image.jpg" alt="foto"/>
+   
+        <header>
+            <h2>Grigorovich Tatyana</h2>
+            <h3>Junior Frontend Developer</h3>
+            
+        </header>
+
+        <article>
+            <table >
+    <tr>
+        <td><h4>Contacts</h4></td>
+    <td>
+        <ul>
+            <li>Tel: +7 963 318-00-31</li>
+            <li>E-mail: gtm003@gmail.com</li>
+        </ul>
+    </td>
+
+  </tr>
+  
+    <tr>
+    <td><h4>Description</h4></td>
+    
+        <td><p>I am looking for a Junior Frontend Developer position with opportunities for professional growth. My previous professional activity was related to the design of different metal structures. I believe that my previous experience combined with persistence in achieving my goals will help me to acquire all necessary skills in a short period of time.</p></td>
+
+    </tr>
+  
+  <tr>
+    <td><h4>Skills</h4></td>
+    <td>
+        <ul>
+            <li>JS Basic</li>
+            <li>HTML Basic</li>
+            <li>CSS Basic</li>
+            <li>C# Basic</li>
+            <li>Git Hub</li>
+        </ul>
+    </td>
+    
+    <tr>
+      <td><h4>Education</h4></td>
+    <td>
+        <p>I am currently studying at the RS School on the JavaScript/Front-end course.</p>
+    </td>  
+    </tr>
+      <tr>
+      <td><h4>English proficiency</h4></td>
+    <td>
+        <p>A2 – Intermediate. I can read technical documentation.</p>
+    </td>
+  </tr> </tr>
+</table>
+        </article>
+  
+</main>
     </div>
   );
 }
